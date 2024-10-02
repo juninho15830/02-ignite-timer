93301c9583aea58ff2b62f5cf4231a55651b8213
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-10-02 18:27:50 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import './App.css'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      Hello World!
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}

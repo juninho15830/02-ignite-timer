4a8c7175497282c9819ca3d296d6558329d62f25
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-10-11 21:11:25 -0300

    update data history

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -31,14 +31,19 @@ export function Home(){
         }
     })
 
-    const { handleSubmit, watch, /*reset*/ } = newCycleForm
+    const { handleSubmit, watch, reset } = newCycleForm
+
+    function handleCreateNewCycle(data: NewCicleFormData) {
+        createNewCycle(data)
+        reset()
+    }
 
     const task = watch('task')
     const isSubmitDisabled = !task
 
     return (
         <HomeContainer>
-            <form onSubmit={handleSubmit(createNewCycle)} action="">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
                 
                     <FormProvider {...newCycleForm}>
                         <NewCycleForm />

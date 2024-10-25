e60ca4b77d149584c52c856bc1e15bf7d556e2d5
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-10-25 15:47:05 -0300

    update index form

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -9,33 +9,33 @@ export function NewCycleForm() {
 
     return (
         <FormContainer>
-                    <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput 
-                        id="task" 
-                        list="task-sugestions"
-                        placeholder="De um nome para o seu projeto"
-                        disabled={!!activeCycle}
-                        {...register('task')}
-                    />
+            <label htmlFor="task">Vou trabalhar em</label>
+            <TaskInput 
+                id="task" 
+                list="task-sugestions"
+                placeholder="De um nome para o seu projeto"
+                disabled={!!activeCycle}
+                {...register('task')}
+            />
 
-                    <datalist id="task-sugestions">
-                        <option value="Projeto-1"></option>
-                        <option value="Projeto-2"></option>
-                    </datalist>
+            <datalist id="task-sugestions">
+                <option value="Projeto-1"></option>
+                <option value="Projeto-2"></option>
+            </datalist>
 
-                    <label htmlFor="minutesAmount">Durante</label>
-                    <MinutesAmountInput 
-                        type="number"
-                        id="minutesAmount" 
-                        placeholder="00"
-                        step={5}
-                        min={5}
-                        max={60}
-                        disabled={!!activeCycle}
-                        {...register('minutesAmount', { valueAsNumber: true })}
-                    />
+            <label htmlFor="minutesAmount">Durante</label>
+            <MinutesAmountInput 
+                type="number"
+                id="minutesAmount" 
+                placeholder="00"
+                step={5}
+                min={5}
+                max={60}
+                disabled={!!activeCycle}
+                {...register('minutesAmount', { valueAsNumber: true })}
+            />
 
-                    <span>minutos</span>
-                </FormContainer>
+            <span>minutos</span>
+        </FormContainer>
     )
 }
